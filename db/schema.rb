bed7bbc9f7980d7db0b9b39db022ffa05bccf752
Arjun Mehta <arjunmehta94@gmail.com>
2016-03-07 06:42:07 +0000

    applies Menu migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307062447) do
+ActiveRecord::Schema.define(version: 20160307063718) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string   "meal"
+    t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
