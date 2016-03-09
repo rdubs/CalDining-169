3bf9b29dd6dbabc44c8638b2260f637973e97a0d
veersuvrat <veer2812@gmail.com>
2016-03-09 02:38:24 -0800

    List of items is now working for each mealtime and location configuration.

--- app/dining_web_parser.rb
+++ app/dining_web_parser.rb
@@ -9,33 +9,40 @@ DOC = Nokogiri::HTML(open(dining_url))
 POSITIONS = {"Crossroads" => 0, "Cafe 3" => 1, "Foothill" => 2, "Clark Kerr" => 3}
 
 BREAKFAST_DATA = DOC.css("td").select{|candidate| candidate.css("b").text == "Breakfast"}
-LUNCH_DATA = DOC.css("td").select{|candidate| candidate.css("b").text == "Lunch"} +
-            DOC.css("td").select{|candidate| candidate.css("b").text == "Brunch"} +
-            DOC.css("td").select{|candidate| candidate.css("b").text == "Lunch/Brunch"}
+LUNCH_DATA = [DOC.css("td").select{|candidate| candidate.css("b").text == "Lunch"}[0],
+            DOC.css("td").select{|candidate| candidate.css("b").text == "Brunch"}[0],
+            DOC.css("td").select{|candidate| candidate.css("b").text == "Lunch/Brunch"}[0],
+            DOC.css("td").select{|candidate| candidate.css("b").text == "Lunch"}[1]]
 DINNER_DATA = DOC.css("td").select{|candidate| candidate.css("b").text == "Dinner"}
 
-#If a location is closed for the particular meal time, you will get back an empty ruby array.
-def get_crossroads_breakfast
-    crossroads_breakfast_items = []
-    BREAKFAST_DATA[CROSSROADS_POSITION].css("a").each{|item| crossroads_breakfast_items << item.text}
-    return crossroads_breakfast_items
-end
-
-def get_cafe_3_breakfast
-    cafe_3_breakfast_items = []
-    BREAKFAST_DATA[CAFE_3_POSITION].css("a").each{|item| cafe_3_breakfast_items << item.text}
-    return cafe_3_breakfast_items
-end
-
-def get_foothill_breakfast
-    foothill_breakfast_items = []
-    BREAKFAST_DATA[CAFE_3_POSITION].css("a").each{|item| foothill_breakfast_items << item.text}
-    return foothill_breakfast_items
-end
+#If a location is closed for the particular meal time, you will get back an EMPTY RUBY ARRAY.
 
 def get_breakfast_items(location)
-    location_breakfast_items = []
     index = POSITIONS[location]
+    if index.nil?
+        raise ArgumentError, "Invalid location name: #{location} entered."
+    end
+    location_breakfast_items = []
     BREAKFAST_DATA[index].css("a").each{|item| location_breakfast_items << item.text}
     return location_breakfast_items
+end
+
+def get_lunch_items(location)
+    index = POSITIONS[location]
+    if index.nil?
+        raise ArgumentError, "Invalid location name: #{location} entered."
+    end
+    location_lunch_items = []
+    LUNCH_DATA[index].css("a").each{|item| location_lunch_items << item.text}
+    return location_lunch_items
+end
+
+def get_dinner_items(location)
+    index = POSITIONS[location]
+    if index.nil?
+        raise ArgumentError, "Invalid location name: #{location} entered."
+    end
+    location_dinner_items = []
+    DINNER_DATA[index].css("a").each{|item| location_dinner_items << item.text}
+    return location_dinner_items
 end
