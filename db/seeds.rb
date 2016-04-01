c671e839a63335785d56c3a42d133023bdd30c38
Michael Peng <michaelpeng@berkeley.edu>
2016-04-01 22:44:35 +0000

    Potential fix to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Crossroads meals 
+crossroads_breakfast = Menu.find_or_create_by(location: 'Crossroads', meal: 'Breakfast')
+crossroads_lunch = Menu.find_or_create_by(location: 'Crossroads', meal: 'Lunch/Brunch')
+crossroads_dinner = Menu.find_or_create_by(location: 'Crossroads', meal: 'Dinner')
+
+# Foothill meals
+foothill_breakfast = Menu.find_or_create_by(location: 'Foothill', meal: 'Breakfast')
+foothill_lunch = Menu.find_or_create_by(location: 'Foothill', meal: 'Lunch/Brunch')
+foothill_dinner = Menu.find_or_create_by(location: 'Foothill', meal: 'Dinner')
+
+# Cafe 3 meals
+cafe_3_breakfast = Menu.find_or_create_by(location: 'Cafe 3', meal: 'Breakfast')
+cafe_3_lunch = Menu.find_or_create_by(location: 'Cafe 3', meal: 'Lunch/Brunch')
+cafe_3_dinner = Menu.find_or_create_by(location: 'Cafe 3', meal: 'Dinner')
+
+# Clark Kerr meals
+clark_kerr_breakfast = Menu.find_or_create_by(location: 'Clark Kerr', meal: 'Breakfast')
+clark_kerr_lunch = Menu.find_or_create_by(location: 'Clark Kerr', meal: 'Lunch/Brunch')
+clark_kerr_dinner = Menu.find_or_create_by(location: 'Clark Kerr', meal: 'Dinner')
+
+
 worker = ParserWorker.new
 worker.perform()
