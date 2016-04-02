b0750eb162d7ce4b4bd1bb1bcb989456541423fe
Michael Peng <michaelpeng@berkeley.edu>
2016-04-02 01:24:17 +0000

    Adding sidetiq functionality

--- app/workers/parser_worker.rb
+++ app/workers/parser_worker.rb
@@ -5,7 +5,12 @@ require 'sidekiq'
 
 class ParserWorker
   include Sidekiq::Worker
-
+  include Sidetiq::Schedulable
+  
+  recurrence backfill: true do
+    daily.hour_of_day(7)
+  end
+  
   def perform(*args)
     @base_url = "http://services.housing.berkeley.edu/FoodPro/dining/static/"
     @menu_path = "todaysentrees.asp"
