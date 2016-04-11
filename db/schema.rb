92322c93f0bf01846fc32dcb713ddd4f8f3b2a10
veersuvrat <veer2812@gmail.com>
2016-04-11 02:31:01 -0700

    Migrated and ran migration to add images table with appropriate has_many relations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330065057) do
+ActiveRecord::Schema.define(version: 20160411091308) do
+
+  create_table "images", force: :cascade do |t|
+    t.string   "filename"
+    t.integer  "item_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "images", ["item_id"], name: "index_images_on_item_id"
+  add_index "images", ["user_id"], name: "index_images_on_user_id"
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
