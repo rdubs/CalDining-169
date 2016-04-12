ebf247618b13ea9c54253722ea14c2c36330ad27
Arjun Mehta <arjunmehta94@gmail.com>
2016-04-12 05:59:11 +0000

    adds admin boolean field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411091308) do
+ActiveRecord::Schema.define(version: 20160412045002) do
 
   create_table "images", force: :cascade do |t|
     t.string   "filename"
@@ -60,6 +60,11 @@ ActiveRecord::Schema.define(version: 20160411091308) do
 
   add_index "items_menus", ["item_id", "menu_id"], name: "index_items_menus_on_item_id_and_menu_id", unique: true
 
+  create_table "items_menus_tables", force: :cascade do |t|
+    t.integer "menu_id"
+    t.integer "item_id"
+  end
+
   create_table "menus", force: :cascade do |t|
     t.string   "meal"
     t.string   "location"
@@ -68,20 +73,21 @@ ActiveRecord::Schema.define(version: 20160411091308) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
