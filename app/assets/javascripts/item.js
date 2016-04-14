a431b32546bd323d80c018ccd3a7ca8f3d0c06de
Hamza Ahmed <[hamza.ah@berkeley.edu]>
2016-04-14 01:12:02 -0700

    Redesigned item view

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,5 +1,5 @@
 $(document).on('ready page:load', function(){
-    $('#upload_button').click(function(){
+    $('#upload-button').click(function(){
         filepicker.pick(
             function(Blob){
                 $.post(upload_post_url, {filename: Blob.url});
