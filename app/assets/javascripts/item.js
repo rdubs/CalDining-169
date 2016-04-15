3a35b56c382f0c09af164f30109b9ccacc684710
veersuvrat <veer2812@gmail.com>
2016-04-14 22:01:49 -0700

    Fix problem with filepicker API key.

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,5 +1,6 @@
 $(document).on('ready page:load', function(){
     $('#upload-button').click(function(){
+        filepicker.setKey(filepicker_key);
         filepicker.pick(
             function(Blob){
                 $.post(upload_post_url, {filename: Blob.url});
