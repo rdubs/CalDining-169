e56f010d676c0d94ba205a356456c671f881f8f6
Arjun Mehta <arjunmehta94@gmail.com>
2016-04-24 05:54:04 +0000

    adds has_and_belongs_to_many for item and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
   has_many :images
+  has_and_belongs_to_many :items, :join_table => :items_users
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
