1e60bfb7e3bd46a20d5a1b07f32c66dddff12777
eric_mh <eric.h@berkeley.edu>
2016-04-27 20:05:03 +0000

    made minor adjustments to the error 500/503 views

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,15 +2,17 @@ class ErrorsController < ApplicationController
     
     def show
         status_code = params[:code] || 500
+        @error_body = "Please double-check the address you typed in."
         case status_code
         when "404"
             @error_message = "The page you searched for doesn't exist!"
         when "403"
-            @error_message = "You don't have permission there."
+            @error_message = "You don't have permission for that."
         when "422"
-            @error_message = "You don't have permission there."
+            @error_message = "You don't have permission for that."
         else
-            @error_message = "Something went wrong."
+            @error_body = ''
+            @error_message = "Something went wrong with the Cal Dining server."
         end
     end
 end
